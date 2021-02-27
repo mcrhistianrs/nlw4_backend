2877b922a9b387a0729b0f3de40a84e3ddf0f680
Maicon Crhistian <maiconcrhistian@MacBook-Pro-de-Maicon.local>
2021-02-26 22:21:53 -0300

    Criado a primeira migration

---
+++ src/database/migrations/1614388829214-CreateUsers.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateUsers1614388829214 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
+import "reflect-metadata";
 import express from 'express';
-
+import './database';
 const app = express();
+
 
 app.get("/users" , (req,res) =>{
     return res.json("Rota /users");

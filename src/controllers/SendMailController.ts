f2e46fe44e1fe4bd3e5425588bce5e5ba05f358e
Maicon Crhistian <maiconcrhistian@MacBook-Pro-de-Maicon.local>
2021-03-01 06:24:00 -0300

    Começado a verificar se o usuário existe pra mandar email

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -1,10 +1,25 @@
 import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { SurveysRepository } from "../repositories/SurveysRepository";
+import { SurveyUsersRepository } from "../repositories/SurveyUsersRepository";
+import { UserRepository } from "../repositories/UsersRepository";
 
 class SendMailController{
 
     async execute(request: Request , response:Response){
         const {email,survey_id} = request.body;
         
+        const userRepostitory = getCustomRepository(UserRepository);
+        const surveyRepository = getCustomRepository(SurveysRepository);
+        const surveyUserRepository = getCustomRepository(SurveyUsersRepository);
+
+        const userAlreadyExists = await userRepostitory.findOne({email});
+
+        if(!userAlreadyExists){
+            return response.status(400).json({
+                error: "User does not exists"
+            })
+        };
     }
 }
 
